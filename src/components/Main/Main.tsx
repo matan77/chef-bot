694b5dc2d535f16d4cbf4cd1cec4b786a2ed6e5d
matan77 <matan54828482@gmail.com>
2025-04-12 16:49:50 +0300

    feat(Main.tsx): update main page

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,25 +1,40 @@
+import { useState } from 'react';
+import { IngredientsList } from '../IngredientsList';
+import { Recipe } from '../Recipe';
 import './style.css';
+import { getRecipeFromMistral } from '../../ai';
 
 const Main = () => {
-	const ingredients = ["Chicken", "Oregano", "Tomatoes"];
+	const [ingredients, setIngredients] = useState<string[]>(["Chicken", "Oregano", "Tomatoes"]);
+	const [recipe, setRecipe] = useState("");
 
-	
+
+	const addIngredient = (formData: FormData): void => {
+		setIngredients([...ingredients, (formData.get("ingredient") as string)]);
+	}
+
+	const getRecipe = async () => {
+		const recipeMarkdown = await getRecipeFromMistral(ingredients) as string;
+		setRecipe(recipeMarkdown);
+	};
+
 	return (
 		<main>
-			<form className="add-ingredient-form">
+			<form action={addIngredient} className="add-ingredient-form">
 				<input
 					type="text"
 					placeholder="e.g. oregano"
 					aria-label="Add ingredient"
+					name="ingredient"
 				/>
 				<button>Add ingredient</button>
 			</form>
-			<ul>
-				{ingredients.map((ingredient, index) =>
-					<li key={index}>{ingredient}</li>)}
-			</ul>
-		</main>
+			{ingredients.length > 0 && <IngredientsList ingredients={ingredients} toggleRecipeShown={getRecipe} />}
+
+			{recipe && <Recipe recipe={recipe} />}
+		</main >
 	)
 }
 
 export { Main };
+
